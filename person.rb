fb181c6654ceab1a859fc8683ddaa6656e10f268
lRebornsl <anthony_vasquez98@hotmail.com>
2023-08-31 12:37:11 -0500

    Modify Person and Decorator classes

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
