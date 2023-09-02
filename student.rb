562df6499681e6c436784f96ef6623b6c06a4df1
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-02 01:07:24 -0500

    Setup associations

--- student.rb
+++ student.rb
@@ -1,17 +1,15 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, name, classroom, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    classroom.students << self unless classroom.students.include?(self)
-    @classroom = classroom
   end
 end
