32e66d4766cb8357af15dc7e14ee1422b0608597
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-05 00:50:41 +0300

    Refactor app remove rental

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'classroom'
-require_relative 'rental'
 require_relative 'list_hud'
 
 class App
+  attr_reader :persons
+
   def initialize
     @persons = []
-    @rentals = []
   end
 
   def run
@@ -74,34 +74,5 @@ class App
     specialization = gets.chomp
     @persons << Teacher.new(name, age, specialization)
     puts 'Teacher created successfully'
-  end
-
-  def rent_book
-    puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
-      puts "#{index}. Title: #{book.title} Author: #{book.author}"
-    end
-    book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number'
-    @persons.each_with_index do |person, index|
-      puts "#{index}. Name: #{person.name} Age: #{person.age}"
-    end
-    person_index = gets.chomp.to_i
-    puts 'Date: '
-    date = gets.chomp
-    @rentals << Rental.new(date, @books[book_index], @persons[person_index])
-    puts 'Book rented successfully'
-  end
-
-  def list_rentals
-    print 'ID of the person: '
-    person_id = gets.chomp.to_i
-    @rentals.each do |rental|
-      if rental.person.id == person_id
-        puts "Date: #{rental.date}, Book: #{rental.book.title}"
-      else
-        puts 'No rentals found for this person'
-      end
-    end
   end
 end
